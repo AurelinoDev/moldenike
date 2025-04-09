d822cace1efa547ed62db37d389b6c2c0b68b9f6
Igor-Aurelino <i.rufino@aluno.senai.br>
2025-04-08 21:12:15 -0300

    Projeto Finalizado

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
 let botao = document.querySelector(".botao-carrinho")
-// document = html
-// querySelector = pegador / selecionador
 
-function mudarVisual(cor, imagem){
-    
+function mudarVisual(cor, imagem) {
     body.style.background = cor
     botao.style.background = cor
+
+    // Remove a animação
+    tenis.classList.remove("animar")
+
+    // Força o reflow pra reiniciar a animação
+    void tenis.offsetWidth
+
+    // Troca a imagem
     tenis.src = imagem
+
+    // Reaplica a classe de animação
+    tenis.classList.add("animar")
 }
+window.addEventListener("load", () => {
+    let tenis = document.querySelector(".imagem-tenis");
+    // Força o reflow para garantir que a animação seja aplicada
+    void tenis.offsetWidth;
+    // Adiciona a classe que dispara a animação
+    tenis.classList.add("animar");
+});
